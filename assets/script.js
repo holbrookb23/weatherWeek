4554ca02e51390b9973a1a881b49b5cd3efec430
Brian Holbrook <brian.holbrook56@gmail.com>
2022-02-01 23:01:18 -0600

    yay

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,62 @@
 
-
-
-
-
-
-function displayWeather(data) {
+function displayWeather(data, cityName) {
     //clear any previous results
     document.querySelector("#currentWeather").innerHTML = "";
+    document.querySelector("#weekForecast").innerHTML = "";
+
+    let historyBtn = document.createElement("button");
+    let historyLink = document.createElement("a")
+    historyLink.innerHTML = cityName;
+    historyLink.setAttribute("href", `file:///C:/Users/Brian/code/weatherWeek/index.html?text=${cityName}`);
+    historyBtn.appendChild(historyLink);
+    document.querySelector("#historyList").appendChild(historyBtn);
 
     const cityTitle = document.createElement("h3");
-    cityTitle.textContent = `${data.timezone} ${data.current.weather[0].main}`;
+    cityTitle.innerHTML = `${cityName} <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png">`;
 
-    const tempPar = document.createElement("p");
-    tempPar.textContent = `Tempature: ${data.current.temp}F`;
+    const today = document.createElement("div");
+    today.innerHTML = `${moment.unix(data.current.dt).format("ddd MMM Do, YYYY")}`;
 
-    const humPar = document.createElement("p");
-    humPar.textContent = `Humidity: ${data.current.humidity}%`;
+    cityTitle.appendChild(today)
 
-    const windPar = document.createElement("p");
-    windPar.textContent = `Wind: ${data.current.wind_speed} MPH`;
-
-    const uviPar = document.createElement("p");
-    uviPar.textContent = `UV Index: ${data.current.uvi}`
+    const currentDiv = document.createElement("div");
+    currentDiv.classList.add("container");
+    currentDiv.classList.add("currentDiv");
+    currentDiv.innerHTML = `<div class="row"><div class="col-6">Tempature:</div><div class="col-6 p-0">${data.current.temp} F</div></div>
+    <div class="row"><div class="col-6">Humidity:</div><div class="col-6 p-0">${data.current.humidity} %</div></div>
+    <div class="row"><div class="col-6">Wind:</div><div class="col-6 p-0">${data.current.wind_speed.toFixed(1)} MPH</div></div>
+    <div class="row"><div class="col-6">UVI:</div><div id="uvi">${data.current.uvi}</div></div>`;
     //color code the uv index to show a color for favorable, moderate, and severe conditions
+    
 
     document.querySelector("#currentWeather").appendChild(cityTitle);
-    document.querySelector("#currentWeather").appendChild(tempPar);
-    document.querySelector("#currentWeather").appendChild(humPar);
-    document.querySelector("#currentWeather").appendChild(windPar);
-    document.querySelector("#currentWeather").appendChild(uviPar);
+    document.querySelector("#currentWeather").appendChild(currentDiv);
+
+    if (data.current.uvi < 3) {
+        document.getElementById("uvi").setAttribute("class", "bg-g col-2");
+    } else if(data.current.uvi >= 3 && data.current.uvi < 6) {
+        document.getElementById("uvi").setAttribute("class", "bg-y col-2");
+    } else {
+        document.getElementById("uvi").setAttribute("class", "bg-r col-2");
+    }
 
     for(i = 1; i < 6; i++) {
+        
         const cardDiv = document.createElement("div");
         cardDiv.classList.add("weatherCard");
+        const day = document.createElement("h4");
+        day.textContent = `${moment.unix(data.daily[i].dt).format("dddd, MMM Do")}`;
+        const weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`)
         const tempParDiv = document.createElement("p");
-        tempParDiv.textContent = `Temp: ${data.daily[i].temp.max}F`;
+        tempParDiv.textContent = `Temp:     ${data.daily[i].temp.max} F`;
         const windParDiv = document.createElement("p");
         windParDiv.textContent = `Wind: ${data.daily[i].wind_speed} MPH`;
         const humParDiv = document.createElement("p");
-        humParDiv.textContent = `Humidity: ${data.daily[i].humidity}%`;
+        humParDiv.textContent = `Humidity:    ${data.daily[i].humidity} %`;
 
+        cardDiv.appendChild(day);
+        cardDiv.appendChild(weatherIcon)
         cardDiv.appendChild(tempParDiv);
         cardDiv.appendChild(windParDiv);
         cardDiv.appendChild(humParDiv);
@@ -48,20 +65,38 @@ function displayWeather(data) {
     }
 }
 
-function getWeather() {
-    
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.4&lon=-94.04&units=imperial&appid=${apiKey}`;
+function getCoordinates(event) {
+    event.preventDefault();
 
-    fetch(weatherUrl)
+    const city = document.getElementById("citySearch").value;
+    const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13aee28c8aa564f699d833de5603c365`;
+
+    fetch(fetchUrl)
     .then(function(response) {
         return response.json();
     })
-    .then(function(data) {
-        console.log(data);
-        displayWeather(data);
+    .then(function(coordinates) {
+        console.log(coordinates);
+        getWeather(coordinates)
     })
-
 
 }
 
-document.querySelector("#searchBtn").addEventListener("click", getWeather);
+function getWeather(coordinates) {
+    const lat = coordinates.coord.lat;
+    const lon = coordinates.coord.lon;
+    const name = coordinates.name
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=13aee28c8aa564f699d833de5603c365`;
+    
+        fetch(weatherUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            displayWeather(data, name);
+        })
+
+}
+
+document.querySelector("#searchBtn").addEventListener("click", getCoordinates);
